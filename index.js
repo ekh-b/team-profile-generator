34cf9f5eb87ca23e562ad2ae2e73a1a4d38ffab4
EB <e.bains@hotmail.co.uk>
2023-05-26 17:24:11 +0100

    Created parent class for employee.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
+const OUTPUT_DIR = path.resolve(__dirname, "output");// Will this folder generate? or do i need to create it?
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
